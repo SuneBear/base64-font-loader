10e65d4e8c8c984e149d18926c7a09f7adfad1f4
Sune Bear <hi@sunebear.com>
2016-08-20 22:25:17 +0800

    REFACTOR(loader): Updated the dictionary & output format

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Base64 font loader for webpack
-This loader will Base64 encode any fonts in your css files.
+# Base64 loader for webpack
+This loader will Base64 encode any files.
 
 ## Usage
 
@@ -22,7 +22,7 @@ module.exports = {
       },
       {
         test: /\.(woff|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'base64-font-loader'
+        loader: 'base64-loader'
       }
     ]
   },
@@ -42,11 +42,7 @@ module.exports = {
 ```css
 @font-face {
   font-family: 'icons';
-  src: url('fonts/icon.eot');
-  src: url('fonts/icon.eot?v=0.0.1') format('eot'),
-       url('fonts/icon.woff?v=0.0.1') format('woff'),
-       url('fonts/icon.ttf?v=0.0.1') format('truetype'),
-       url('fonts/icon.svg?v=0.0.1') format('svg');
+  src: url('fonts/icon.svg?v=0.0.1') format('svg');
   font-weight: normal;
   font-style: normal;
 }
@@ -57,11 +53,7 @@ module.exports = {
 ```css
 @font-face {
   font-family: 'icons';
-  src: url(data:application/vnd.ms-fontobject;charset=utf-8;base64,[BASE_64_STRING]);
-  src: url(data:application/vnd.ms-fontobject;charset=utf-8;base64,[BASE_64_STRING]) format('eot'),
-       url(data:application/font-woff;charset=utf-8;base64,[BASE_64_STRING]) format('woff'),
-       url(data:application/x-font-ttf;charset=utf-8;base64,[BASE_64_STRING]) format('truetype'),
-       url(data:image/svg+xml;charset=utf-8;base64,[BASE_64_STRING]) format('svg');
+  src: url(data:image/svg+xml;charset=utf-8;base64,[BASE_64_STRING]) format('svg');
   font-weight: normal;
   font-style: normal;
 }
--- index.js
+++ index.js
@@ -1,32 +1,30 @@
-/*
-	MIT License http://www.opensource.org/licenses/mit-license.php
-	Author Nick Randall
-*/
-var loaderUtils = require('loader-utils');
+/**
+ * REF: https://github.com/nicksrandall/base64-font-loader
+ */
 
-var dictionary = {
-	'woff': 'application/font-woff',
-	'woff2': 'application/font-woff2',
-	'eot': 'application/vnd.ms-fontobject',
-	'ttf': 'application/x-font-ttf',
-	'otf': 'font/opentype',
-	'svg': 'image/svg+xml'
-};
+const loaderUtils = require('loader-utils')
 
-module.exports = function(content) {
-	this.cacheable && this.cacheable();
-	var query = loaderUtils.parseQuery(this.query);
-	var ext = loaderUtils.interpolateName(this, "[ext]", {
-		context: query.context || this.options.context,
-		content: content,
-		regExp: query.regExp
-	}).toLowerCase();
-	if (!dictionary.hasOwnProperty(ext)) {
-		throw new Error(ext + ' font type is not supported');
-	}
-	var url = 'data:' + dictionary[ext] + ';charset=utf-8;base64,';
-	url += content.toString('base64');
-	return 'module.exports = ' + JSON.stringify(url);
+const dictionary = {
+  'png': 'image/png',
+  'gif': 'image/gif',
+  'jpg': 'image/jpeg',
+  'svg': 'image/svg+xml'
 }
 
-module.exports.raw = true;
+module.exports = function(content) {
+  this.cacheable && this.cacheable()
+  var query = loaderUtils.parseQuery(this.query)
+  var ext = loaderUtils.interpolateName(this, "[ext]", {
+    context: query.context || this.options.context,
+    content: content,
+    regExp: query.regExp
+  }).toLowerCase()
+  if (!dictionary.hasOwnProperty(ext)) {
+    throw new Error(ext + ' font type is not supported')
+  }
+  var url = 'data:' + dictionary[ext] + ';base64,'
+  url += content.toString('base64')
+  return 'module.exports = ' + JSON.stringify(url)
+}
+
+module.exports.raw = true
